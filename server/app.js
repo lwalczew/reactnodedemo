1061cf8b6de5fba2448a541214d287a8d7b8095d
Luke Walczewski <luke@schedulingad.com>
2017-11-30 22:24:08 -0500

    successfully connected to MongoDB on mLab

--- server/app.js
+++ server/app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const routes = require('./routes/index');
 const newsRoute = require('./routes/news');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 let app = express();
 const PORT = process.env.PORT || 5000;
+
+const dbURL = process.env.MONGO_DB_URL
+
+mongoose.connect(dbURL, function(err) {
+  if(err) {
+    console.log('Error connecting to: '+ dbURL)
+  } else {
+    console.log('Connected to: ' + dbURL)
+  }
+})
 
 app.use(cors());
 app.options('*', cors());
--- server/routes/news.js
+++ server/routes/news.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const fakeNews = [{
-  id: '1',
+  id: 1,
   title: 'Mad owl chases car',
   teaser: 'Mad owl seen tormenting drivers in Morecambe',
   body: 'This is some body text regarding the news story of the mad owl tormenting drivers in Morecambe'
 }, {
-  id: '2',
+  id: 2,
   title: 'Owl stowaway',
   teaser: 'Despicable owl impersonates passenger to board flight to Luton',
   body: 'This is some body text regarding the news story of the owl making its way onto a domestic flight to luton'
 }, {
-  id: '3',
+  id: 3,
   title: 'Owl steals pork pie',
   teaser: 'This morning a rogue owl stole a pork pie from a shop in Swindon.',
   body: 'This is some body text regarding the news story of the owl stealing a pork pie from a shop in swindon'
@@ -25,7 +25,7 @@ router.get('/', function(req, res, next) {
 })
 
 router.get('/:id', function(req, res, next) {
-  const id = req.params.id;
+  const id = parseInt(req.params.id, 10);
   const picked = fakeNews.find(o => o.id === id);
   res.status(200).send({
     data: picked
